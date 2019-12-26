67677785fa7ef1c37a8374d6bf697a07406af1fc
zdm <zdm@softvisio.net>
2019-12-26 21:36:12 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,55 @@ module.exports = {
                     "loader": "babel-loader",
                 },
             },
+            {
+                "test": /\.css$/i,
+                "use": ["style-loader", "css-loader"],
+            },
+            {
+                "test": /\.(svg)(\?.*)?$/,
+                "use": [
+                    {
+                        "loader": "file-loader",
+                        "options": {
+                            "name": "img/[name].[hash:8].[ext]",
+                        },
+                    },
+                ],
+            },
+            {
+                "test": /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+                "use": [
+                    {
+                        "loader": "url-loader",
+                        "options": {
+                            "limit": 4096,
+                            "fallback": {
+                                "loader": "file-loader",
+                                "options": {
+                                    "name": "fonts/[name].[hash:8].[ext]",
+                                },
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                "test": /\.(png|jpe?g|gif|webp)(\?.*)?$/,
+                "use": [
+                    {
+                        "loader": "url-loader",
+                        "options": {
+                            "limit": 4096,
+                            "fallback": {
+                                "loader": "file-loader",
+                                "options": {
+                                    "name": "img/[name].[hash:8].[ext]",
+                                },
+                            },
+                        },
+                    },
+                ],
+            },
         ],
     },
 };
