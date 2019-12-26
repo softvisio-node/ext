c78139228b4d00a334b95cca26a6ceb399740223
zdm <zdm@softvisio.net>
2019-12-26 22:26:06 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
+const HtmlWebpackPlugin = require( "html-webpack-plugin" );
+
 module.exports = {
     "entry": "./lib/index.js",
+    "plugins": [
+        new HtmlWebpackPlugin( {
+            "hash": true,
+            "filename": "index.html", //relative to root of the application
+        } ),
+    ],
     "module": {
         "rules": [
             {
