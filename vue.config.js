53d76726b78bf755ece42986d959087e98e53cf4
zdm <zdm@softvisio.net>
2019-12-28 14:54:10 +0200

    settings changed

--- vue.config.js
+++ vue.config.js
@@ -34,10 +34,18 @@ module.exports = {
     },
 
     "chainWebpack": ( config ) => {
+        // configure html minification, https://github.com/kangax/html-minifier#options-quick-reference
+        config.plugin( "html" ).tap( ( args ) => {
+            args[0].minify.removeAttributeQuotes = false;
+
+            return args;
+        } );
+
         if ( process.env.NODE_ENV === "production" ) {
             //
         }
         else if ( process.env.NODE_ENV === "development" ) {
+            // disable minification in development mode
             config.optimization.minimize( false );
             // config.optimization.delete( "splitChunks" );
         }
