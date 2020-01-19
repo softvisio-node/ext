b9cdf11e35f97979a0046d9b7089556592324ab2
zdm <zdm@softvisio.net>
2020-01-19 16:54:32 +0200

    gitignore

--- vue.config.js
+++ vue.config.js
@@ -2,38 +2,52 @@
 
 const webpack = require( "webpack" );
 
-module.exports = {
+var config = {
+    "outputDir": "www",
+
     "devServer": {
         "contentBase": "build",
         "hot": true,
         "historyApiFallback": true,
         "host": "0.0.0.0",
-        "port": "8080",
+        "port": "80",
         "disableHostCheck": false,
         "compress": false,
         "inline": true,
         "stats": "none",
     },
 
+    "publicPath": "",
+
+    "pluginOptions": {
+        // WebpackBundleAnalyzer, https://github.com/webpack-contrib/webpack-bundle-analyzer#options-for-plugin
+        "webpackBundleAnalyzer": {
+            "openAnalyzer": false,
+        },
+    },
+
     // disable lint on development, enable on production
     "lintOnSave": process.env.NODE_ENV === "production" ? "error" : false,
 
+    // NOTE slow
+    "productionSourceMap": false,
+
     "configureWebpack": ( config ) => {
-        config.entry = "./lib/latest.js";
+        // aliases
+        config.resolve.alias["#ext"] = "@softvisio/ext/lib/ext-" + process.env.EXT_VERSION + ".js";
+        config.resolve.alias["#ewc"] = "@softvisio/ext/share/ewc-" + process.env.EWC_VERSION;
+        config.resolve.alias["#swc"] = "@softvisio/web-components/lib";
 
+        // global vars
         config.plugins.push( new webpack.ProvidePlugin( {
-            "Ext": "@softvisio/ext",
+            "Ext": config.resolve.alias["#ext"],
         } ) );
-
-        if ( process.env.NODE_ENV === "production" ) {
-            //
-        }
-        else if ( process.env.NODE_ENV === "development" ) {
-            //
-        }
     },
 
     "chainWebpack": ( config ) => {
+        // disable WebpackBundleAnalyzer
+        // config.plugins.delete( "webpack-bundle-analyzer" );
+
         if ( process.env.NODE_ENV === "production" ) {
             // configure html minification, https://github.com/kangax/html-minifier#options-quick-reference
             config.plugin( "html" ).tap( ( args ) => {
@@ -42,10 +56,7 @@ module.exports = {
                 return args;
             } );
         }
-        else if ( process.env.NODE_ENV === "development" ) {
-            // disable minification in development mode
-            config.optimization.minimize( false );
-            // config.optimization.delete( "splitChunks" );
-        }
     },
 };
+
+module.exports = config;
