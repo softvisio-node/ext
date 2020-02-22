3cb6796892ae9d95942c41c523579338434e74cf
zdm <zdm@softvisio.net>
2020-02-22 10:46:07 +0200

    ewc updated

--- share/ewc-v7.2.0/runtime/webcomponentsbase.js
+++ share/ewc-v7.2.0/runtime/webcomponentsbase.js
@@ -1,4 +1,4 @@
-//Fri Feb 21 2020 07:54:31 GMT-0500 (Eastern Standard Time)
+//Fri Feb 21 2020 21:05:44 GMT-0500 (Eastern Standard Time)
 
 import {
   doProp,
@@ -245,6 +245,13 @@ export default class WebComponentsBaseComponent extends HTMLElement {
     me.A.ext = Ext.create(me.A.o);
     me.cmp = me.A.ext;
     me.ext = me.A.ext;
+
+    me.dispatchEvent(new CustomEvent('created', {
+       detail: {
+         cmp: me.A.ext,
+       }
+    }))
+
     this.doChildren(this);
 
     if (isApplication) {
