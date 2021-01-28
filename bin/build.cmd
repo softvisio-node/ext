eb71567b5360c12029fb60934d333a44d6c8c4c7
zdm <zdm@softvisio.net>
2021-01-28 11:03:16 +0200

    ext build script updated

--- bin/build.cmd
+++ bin/build.cmd
@@ -2,23 +2,26 @@
 
 setlocal
 
-set TARGET=d:\downloads
+set TARGET=%~dp0
+set NAME=ext-build
+
+set BUILD_DIR=%TARGET%\%NAME%
 
 pushd %TARGET%
 
-call npm i -g @vue/cli
+npx vue create --default %NAME%
 
-call vue create --default ext-build
+exit /B
 
-cd ext-build
+cd %BUILD_DIR%
 
-call npm install @sencha/ext @sencha/ext-modern @sencha/ext-modern-theme-material
-call npm install @sencha/ext-web-components-modern @sencha/ext-webpack-plugin
+npm install @sencha/ext @sencha/ext-modern @sencha/ext-modern-theme-material
+npm install @sencha/ext-web-components-modern @sencha/ext-webpack-plugin
 
-cp "%~dp0/vue.config.js" "%TARGET%\ext-build\vue.config.js"
+cp "%~dp0/vue.config.js" "%BUILD_DIR%\vue.config.js"
 
 :: TODO not exited
-call npx vue-cli-service build
+npx vue-cli-service build
 
 :: prepare resources
 del build\ext\resources\Readme.md
