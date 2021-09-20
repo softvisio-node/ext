2b7c7f2101ae2fd1b44ae2558783425b76522710
zdm <zdm@softvisio.net>
2021-09-20 14:03:36 +0300

    fix: boolean props

--- resources/ewc-7.4.0/common/util.js
+++ resources/ewc-7.4.0/common/util.js
@@ -1,12 +1,44 @@
-import { filterProp as filterPropOrig } from "./util-orig.js";
+import { filterProp as filterPropOrig, doProp as doPropOrig } from "./util-orig.js";
 
 export * from "./util-orig.js";
 
-const BOOL_ATTRS = new Set( ["hidden"] );
+// known HTML boolean args (@vue/shared/isBooleanAttr):
+// itemscope,allowfullscreen,formnovalidate,ismap,nomodule,novalidate,readonly,async,autofocus,autoplay,controls,default,defer,disabled,hidden,loop,open,required,reversed,scoped,seamless,checked,muted,multiple,selected
+
+// knoext ExtJS boolean props, which conflicts with the HTML boolean props
+const BOOL_PROPS = new Set( ["checked", "disabled", "hidden"] );
 
 // patch for vue3 boolean attributes coercion
 export function filterProp ( propertyValue, property, me ) {
-    if ( BOOL_ATTRS.has( property ) ) return propertyValue === "";
+
+    // if attribute is set value is ""
+    if ( BOOL_PROPS.has( property ) ) return propertyValue === "";
 
     return filterPropOrig( propertyValue, property, me );
+}
+
+export function doProp ( me, prop ) {
+    if ( !BOOL_PROPS.has( prop ) ) return doPropOrig( me, prop );
+
+    Object.defineProperty( me, prop, { "configurable": true } );
+
+    doPropOrig( me, prop );
+
+    const { get, set } = Object.getOwnPropertyDescriptor( me, prop );
+
+    Object.defineProperty( me, prop, {
+        "configurable": false,
+        get,
+        "set": function ( val ) {
+            doSet( set, me, prop, val );
+        },
+    } );
+}
+
+function doSet ( doSetOrig, me, prop, val ) {
+    if ( BOOL_PROPS.has( prop ) ) {
+        if ( typeof val === "boolean" ) val = val.toString();
+    }
+
+    doSetOrig( val );
 }
