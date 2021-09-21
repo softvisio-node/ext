4defe2b4de869135c179a6778158ee68f27ebdb3
zdm <zdm@softvisio.net>
2021-09-21 08:02:29 +0300

    fix: boolean props

--- resources/ewc-7.4.0/common/util.js
+++ resources/ewc-7.4.0/common/util.js
@@ -2,23 +2,25 @@ import { filterProp as filterPropOrig, doProp as doPropOrig } from "./util-orig.
 
 export * from "./util-orig.js";
 
-// known HTML boolean args (@vue/shared/isBooleanAttr):
-// itemscope,allowfullscreen,formnovalidate,ismap,nomodule,novalidate,readonly,async,autofocus,autoplay,controls,default,defer,disabled,hidden,loop,open,required,reversed,scoped,seamless,checked,muted,multiple,selected
+// known VueJS boolean props (@vue/shared/isBooleanAttr):
+// allowfullscreen, async, autofocus, autoplay, checked, controls, default, defer, disabled, formnovalidate, hidden, ismap, itemscope, loop, multiple, muted, nomodule, novalidate, open, readonly, required, reversed, scoped, seamless, selected
 
-// knoext ExtJS boolean props, which conflicts with the HTML boolean props
+// known ExtJS boolean props, which are conflicts with the VueJS boolean props
 const BOOL_PROPS = new Set( ["checked", "disabled", "hidden"] );
 
 // patch for vue3 boolean attributes coercion
+// if boolean attribute is set, vue3 converts it value to "", here we need to convert it back to the boolean
 export function filterProp ( propertyValue, property, me ) {
 
-    // if attribute is set value is ""
+    // if attribute is set - value is ""
     if ( BOOL_PROPS.has( property ) ) return propertyValue === "";
 
     return filterPropOrig( propertyValue, property, me );
 }
 
 export function doProp ( me, prop ) {
-    if ( !BOOL_PROPS.has( prop ) ) return doPropOrig( me, prop );
+
+    // if ( !BOOL_PROPS.has( prop ) ) return doPropOrig( me, prop );
 
     Object.defineProperty( me, prop, { "configurable": true } );
 
@@ -35,10 +37,9 @@ export function doProp ( me, prop ) {
     } );
 }
 
+// VueJS set properties as is, without any coercion, for boolean props we need stringify boolean values
 function doSet ( doSetOrig, me, prop, val ) {
-    if ( BOOL_PROPS.has( prop ) ) {
-        if ( typeof val === "boolean" ) val = val.toString();
-    }
+    if ( typeof val === "boolean" ) val = val.toString();
 
     doSetOrig( val );
 }
