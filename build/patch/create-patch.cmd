1bcf628bf36199f9810dc01cec18ee82693478fd
zdm <zdm@softvisio.net>
2022-07-02 15:14:28 +0300

    fix: patch refactored

--- build/patch/create-patch.cmd
+++ build/patch/create-patch.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-diff --recursive --no-ignore-file-name-case -u ../node_modules node_modules | grep -v "^Only in" > patch
+diff --recursive --no-ignore-file-name-case -u ../node_modules node_modules_patch | grep -v "^Only in" > patch
