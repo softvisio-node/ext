a2cdf389d997f389b460b1cdcbdda81572ed6862
zdm <zdm@softvisio.net>
2022-09-02 19:30:20 +0300

    fix: patch

--- build/patch/apply-patch.cmd
+++ build/patch/apply-patch.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-patch --dry-run --forward -p1 -i patch/patch
+patch -d "%~dp0%.." --dry-run --forward -p1 -i patch/patch
 
-patch --quiet --forward -p1 -i patch/patch
+patch -d "%~dp0%.." --quiet --forward -p1 -i patch/patch
--- build/patch/revert-patch.cmd
+++ build/patch/revert-patch.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-patch --dry-run --forward --reverse -p1 -i patch/patch
+patch -d "%~dp0%.." --dry-run --forward --reverse -p1 -i patch/patch
 
-patch --quiet --forward --reverse -p1 -i patch/patch
+patch -d "%~dp0%.." --quiet --forward --reverse -p1 -i patch/patch
