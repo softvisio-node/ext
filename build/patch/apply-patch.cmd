1fa1b2285c71836e20ba8ae800feaa2721e8459d
zdm <zdm@softvisio.net>
2022-09-02 19:45:30 +0300

    fix: patch

--- build/patch/apply-patch.cmd
+++ build/patch/apply-patch.cmd
@@ -1,5 +1,5 @@
 @echo off
 
-patch -d "%~dp0%.." --dry-run --forward -p1 -i patch/patch
+patch -d "%~dp0%.." --dry-run --forward -p1 -i patch/patch || exit /B 1
 
-patch -d "%~dp0%.." --quiet --forward -p1 -i patch/patch
+patch -d "%~dp0%.." --quiet --forward -p1 -i patch/patch || exit /B 1
