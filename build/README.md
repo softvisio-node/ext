be56c19b92f80beac29b71d5dcd97617d49193e9
zdm <zdm@softvisio.net>
2025-04-22 06:08:53 +0300

    feat: extjs 7.9.0

--- build/README.md
+++ build/README.md
@@ -1,6 +1,6 @@
 # Build
 
-Ext-addons package <https://trials.sencha.com/ext/ext-addons-7.8.0-trial.zip>.
+Ext-addons package <https://trials.sencha.com/ext/ext-addons-7.9.0-trial.zip>.
 
 ```sh
 npm run build
--- src/ext-7.8.0.js
+++
@@ -1,5 +0,0 @@
-import locale from "#vue/locale";
-
-await locale.add( language => import( /* webpackChunkName: "locales/[request]" */ "./ext-7.x.x/locales/" + language + ".po" ) );
-
-await import( /* webpackChunkName: "ext" */ "./ext-7.x.x/index.js" );
